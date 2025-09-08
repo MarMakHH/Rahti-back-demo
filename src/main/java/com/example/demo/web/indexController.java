18afa5c1c78617b8df659c54f0b9ff881560d275
bhi021@myy.haaga-helia.fi <bhi021@myy.haaga-helia.fi>
2025-09-08 12:57:01 +0300

    show env value on page

--- src/main/java/com/example/demo/web/indexController.java
+++ src/main/java/com/example/demo/web/indexController.java
@@ -1,6 +1,7 @@
 package com.example.demo.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,10 +13,14 @@ public class indexController {
 
     @Autowired
     private MessageRepository repo;
+
+    @Autowired
+    private Environment env;
     
     @GetMapping("/")
     public String getIndex(Model model) {
         model.addAttribute("messages", repo.findAll());
+        model.addAttribute("secret", env.getProperty("my.secret"));
         return "index";
     }
 }
